2782473719b62ce5e475bf04f65b334e0b623629
86co <fuji2o3lq@gmail.com>
2019-06-03 14:23:07 +0900

    improve readability

--- em.py
+++ em.py
@@ -25,91 +25,113 @@ to_int = {
     C: 3
 }
 
-D = collections.namedtuple("D", "X, C")
-GS_result = collections.namedtuple("GS_Result", "motif, score")    #Global search result
+seed_prior_weight = 1.0
+motif_width = 9
 
+D = collections.namedtuple("D", "X, C")
+GS_result = collections.namedtuple("GS_Result", "psfm, score")    #Global search result
+
+class Params():
+    def __init__(self, dataset, lmbd=0.8):
+        self.theta_m = np.zeros((len(SGM),motif_width))
+        self.theta_b = self.calc_fm(dataset)
+        self.lmbd    = lmbd
+        self.gmm     = self.calc_gmm(dataset)
+        
+    def calc_fm(self, dataset):
+        num=[0,0,0,0]
+        den=0
+        for d in dataset:
+            for x in d.X:
+                num[to_int[x]]+=1
+                den+=1
+        theta_b = np.array([num[0], num[1], num[2], num[3]], float)/den if den>0 else None
+        return theta_b
+        
+    def calc_gmm(self, dataset):
+        den=0
+        num=0
+        for d in dataset:
+            den+=1
+            if d.C==1: num+=1 
+        gmm = num/den if den>0 else None
+        return gmm
+            
 def main():
-    Entrez.email = "fujioka86co@gmail.com"
+#    Entrez.email = "fujioka86co@gmail.com"
     
     #sample set
     dna0=list("CGCAGTACGGCTATGTCAT")
     dna1=list("CGTAGCTGAGCTGCTAGCT")
+    dna2=list("TACTGCGTACTGCGATCGC")
+    dna3=list("GCGTATGCGTACTGATACG")
     
     dataset=[]
     dataset.append(D(dna0,1))
-    dataset.append(D(dna1,0))
+    dataset.append(D(dna1,1))
+    dataset.append(D(dna2,0))
+    dataset.append(D(dna3,0))
     
-    gmm = calc_gmm(dataset)
-    theta_b = calc_fm(dataset)
+    params = Params(dataset)
     
-    width = 5
-    lmbd = 0.8
-    
-    b = 1.0
-    
-    motif, maxScore = global_search(dataset, gmm, theta_b, width, lmbd, b)
-    print(motif, maxScore)
-    
-def calc_gmm(dataset):
-    den=0
-    num=0
-    for d in dataset:
-        den+=1
-        if d.C==1: num+=1 
-    gmm = num/den if den>0 else None
-    return gmm
+    params.theta_m = global_search(dataset, params)
+    print(params.theta_m)
+    params.theta_m, maxScore = local_search(dataset, params)
 
-def calc_fm(dataset):
-    num=[0,0,0,0]
-    den=0
-    for d in dataset:
-        for x in d.X:
-            num[to_int[x]]+=1
-            den+=1
-    theta_b = np.array([num[0], num[1], num[2], num[3]], float)/den if den>0 else None
-    return theta_b
-
-def global_search(dataset, gmm, theta_b, width, lmbd, b):
+def global_search(dataset, params):
     gs_result=[]
     for d in dataset:
-        for i in range(len(d.X)-width+1):
-            motif = d.X[i:i+width]
-            theta_m = to_psfm(motif, b)
-            score = objective(lmbd, theta_m, gmm, theta_b, dataset)
-            gs_result.append(GS_result(motif, score))
-    motif_maxScore=""
+        for i in range(len(d.X)-motif_width+1):
+            motif = d.X[i:i+motif_width]
+            params.theta_m = to_psfm(motif)
+            score = objective(dataset, params)
+            gs_result.append(GS_result(params.theta_m, score))
+#            print(motif, score)
+    theta_m_maxScore=""
     maxScore=0
     for res in gs_result:
         if res.score>maxScore:
-            motif_maxScore=res.motif
+            theta_m_maxScore=res.psfm
             maxScore=res.score
-    return motif_maxScore, maxScore
+    return theta_m_maxScore
         
-def to_psfm(motif, b):
-    theta_m = np.zeros((len(SGM),len(motif)))
+def to_psfm(motif):
+    theta_m = np.zeros((len(SGM),motif_width))
     for i, m_i in enumerate(motif):
         for a, sgm_a in enumerate(SGM):
             theta_m[a,i] = float(m_i==sgm_a)
-    theta_m+=b/len(SGM)
-    theta_m/=1+b
+    theta_m += seed_prior_weight/len(SGM)
+    theta_m /= 1+seed_prior_weight
     return theta_m
         
-def objective(lmbd, theta_m, gmm, theta_b, dataset):
-    v = gmm*lmbd/(1-gmm*lmbd)
-    q = gmm*(1-lmbd)/(1-gmm*lmbd)
+def objective(dataset, params):
+    v = params.gmm*params.lmbd/(1-params.gmm*params.lmbd)
+    q = params.gmm*(1-params.lmbd)/(1-params.gmm*params.lmbd)
     score = 0
+    
     for d in dataset:
         mu = 0
-        for i in range(len(d.X)-theta_m.shape[1]+1):
+        for i in range(len(d.X)-motif_width+1):
             prod=1
-            for j in range(theta_m.shape[1]):
-                prod*=theta_m[to_int[d.X[i+j]],j]/theta_b[to_int[d.X[i+j]]]
+            for j in range(motif_width):
+                prod*=params.theta_m[to_int[d.X[i+j]],j]/params.theta_b[to_int[d.X[i+j]]]
             mu+=prod
-        mu/=len(d.X)-theta_m.shape[1]+1
+        mu/=len(d.X)-params.theta_m.shape[1]+1
         sig_y = 1/(1+1/v*mu)
         p = (1-q)*(1-sig_y) if d.C==0 else sig_y+q*(1-sig_y)
         score+=p
+        
     return score
+
+def local_search(dataset, params):
+    w = np.zeros((len(SGM), motif_width))
+    for i in range(motif_width):
+        for a in range(len(SGM)):
+            w[a,i] = math.log(params.theta_m[a,i]/params.theta_b[a])
+    
+#    print(w)
+    
+    return w, 0
     
 if __name__ == "__main__":
     main()
